3b3c792b9ab1e309bd2b81ac959a2b1259abe6a1
B1aho <wheelsurgut00@gmail.com>
2024-12-25 14:18:50 +0300

    Change data into object of maps

--- src/todoService.js
+++ src/todoService.js
@@ -3,30 +3,19 @@ import { TodoItem } from "./todoItem";
 import { serialize, deserialize } from "./serialize";
 
 let data = {
-    projects: [],
-    sections: [],
-    todos: [],
+    projects: new Map(),    // Хранит проекты с id как ключом
+    sections: new Map(),    // Хранит секции с id как ключом
+    todos: new Map(),       // Хранит todos с id как ключом
 }
-/**
- * Модуль в котором происходит оснвоное управление логикой приложения (DOM separately)
- * 
- * Метод который создает новый проект, и сохраняет в data
- * Метод который создает новую секцию, и сохраняет в data
- * Тоже самое c todo, сохранять в data, так как todo возвращается из createTodo
- * 
- * Пусть с prompt запрашивает все данные
- * 
- * Метод который сохраняет и достает из LocalStorage
- * 
- * Протестировать базовую реализацию
- */
+
+// Переделать под map логику
 
 function createProject() {
     const title = prompt("Enter project title:")
     const desc = prompt("Enter project description:")
     const color = prompt("Enter project color:")
     const proj = new Project(title, desc, color)
-    data.projects.push(proj)
+    data.projects.set(proj.id, proj)
     return proj
 }
 
@@ -34,37 +23,37 @@ function createTodo(parent) {
     const title = prompt("Enter todo title:")
     const desc = prompt("Enter todo description:")
     const todo = parent.createTodo(title, desc)
-    data.todos.push(todo)
+    data.todos.set(todo.id, todo)
     return todo
 }
 
 function createSection(project) {
     const title = prompt("Enter ssection title:")
     const section = project.createSection(title)
-    data.sections.push(section)
+    data.sections.set(section.id, section)
     return section
 }
 
 function saveApp() {
-    data.projects.forEach((p, idx, arr) => arr[idx] = serialize(p))
-    data.sections.forEach((s, idx, arr) => arr[idx] = serialize(s))
-    data.todos.forEach((t, idx, arr) => arr[idx] = serialize(t))
+    data.projects.forEach((p, key, map) => map.set(key, serialize(p)))
+    data.sections.forEach((s, key, map) => map.set(key, serialize(s)))
+    data.todos.forEach((t, key, map) => map.set(key, serialize(t)))
     localStorage.setItem("todoApp", JSON.stringify(data))
 }
 
 function getApp() {
     const lastData = JSON.parse(localStorage.getItem("todoApp"))
-    lastData.projects.forEach((p, idx, arr) => arr[idx] = deserialize(p, Project.prototype))
-    lastData.sections.forEach((s, idx, arr) => arr[idx] = deserialize(s, Section.prototype))
-    lastData.todos.forEach((t, idx, arr) => arr[idx] = deserialize(t, TodoItem.prototype))
+    lastData.projects.forEach((p, key, map) => map.set(key, deserialize(p, Project.prototype)))
+    lastData.sections.forEach((s, key, map) => map.set(key, deserialize(s, Section.prototype)))
+    lastData.todos.forEach((t, key, map) => map.set(key, deserialize(t, TodoItem.prototype)))
     data = lastData
 }
 
-const p = createProject()
-createTodo(p)
-createTodo(p)
-const s = createSection(p)
-createTodo(s)
-createTodo(s)
-saveApp()
-getApp()
+// Продумать remove, рекурсивный или что-то типо. Логику удаления
+// Он должен получать id, найти этот элемент. Допусти это проект, тогда он должен проверить есть ли todo и
+// удалить их сначала, потом проверить есть ли секции и удалить их (т.е. удалить все объекты с такими id из data
+// и установить свойство в null), 
+// если секции и todo установлены в null или не существуют, тогда удаляем сам объект из data, но по идее если
+// объект не проджект, а секция то мы должны проверить еще свойство parentId, если там что-то есть, то перейти в родителя и удалить id удаляемого элемента
+
+// добавить тип в генерацию id
